5caa0413a96f09d49b85ed4c09301450afd515cb
HoMinhDang <119878480+HoMinhDang@users.noreply.github.com>
2023-03-24 15:37:50 +0700

    Update Board.h

--- Board.h
+++ Board.h
@@ -13,10 +13,11 @@
 struct Board
 {
 		int n_row, n_column;
+		int n_type;
 		int ** _board;		
 		
 		// constructor và destructor
-		Board(int _n_row, int _n_column, int n_type);
+		Board(int _n_row = 9, int _n_column = 16, int _n_type = 26);
 		~Board();
 		
 		// Các hàm lấy giá trị
@@ -28,11 +29,29 @@ struct Board
 		void removeCell(int _x, int _y); 
 		void setCell(int _x, int _y, int type);
 		void initBoard(int _n_row, int _n_column, int n_type);
-		
+			// Hàm suffleBoard -> tương tự initBoard()
+		void suffleBoard();
+			// Hàm đưa ra suggestion -> trả về vector chứa 2 cái pair hoặc pair<pair,pair>
+		void suggestBoard(std::pair <int,int> &start, std::pair <int,int> &end);
+			// Hàm xóa 2 ô khi chọn được theo kiểu bình thường
+		void removeBoard_0(std::pair <int,int> start, std::pair <int,int> end);
+			// Hàm xóa 2 ô khi chọn được, sau đó thụt qua trái
+		void removeBoard_1(std::pair <int,int> start, std::pair <int,int> end);
+			// Hàm xóa 2 ô khi chọn được, sau đó thụt lên trên
+		void removeBoard_2(std::pair <int,int> start, std::pair <int,int> end);
+			// Hàm xóa 2 ô khi chọn được, sau đó thụt qua phải
+		void removeBoard_3(std::pair <int,int> start, std::pair <int,int> end);
+			// Hàm xóa 2 ô khi chọn được, sau đó thụt xuống dưới
+		void removeBoard_4(std::pair <int,int> start, std::pair <int,int> end);
+
 		
 		// Hàm kiểm tra tính logic của game
 		bool canConnect(std::pair<int,int> start, std::pair<int,int> end);
 		std::vector <std::pair<int,int>> findPath(std::pair<int,int> start, std::pair<int,int> end);
+			// Hàm kiểm tra có 2 ô nào có thể kết nối không checkBoard -> 4 vòng for 
+		bool checkBoard();
+			
+		
 		
 };
 
